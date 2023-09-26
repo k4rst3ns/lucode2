fcff4a55d56cd9ff3260b849cb6b5c84f7d77292
Kristine Karstens <33092354+k4rst3ns@users.noreply.github.com>
2023-09-26 15:02:06 +0200

    add returning of package version to piamPackages

--- R/piamPackages.R
+++ R/piamPackages.R
@@ -2,10 +2,11 @@
 #'
 #' Fetches the names of packages available on https://pik-piam.r-universe.dev/ui#builds
 #'
+#' @param version return version information (if TRUE) - default is FALSE
 #' @return A character vector of names of packages available on https://pik-piam.r-universe.dev/ui#builds
 #'
 #' @export
-piamPackages <- function() {
+piamPackages <- function(version = FALSE) {
   packagesUrl <- "https://pik-piam.r-universe.dev/src/contrib/PACKAGES"
   fallback <- c("blackmagicc", "citation", "demystas", "edgeTransport", "edgeTrpLib",
                 "GDPuc", "gdx", "gdxrrw", "gms", "goxygen", "HARr", "iamc", "limes",
@@ -18,7 +19,16 @@ piamPackages <- function() {
                 "piamModelTests", "piktests", "quitte", "r2databus", "regressionworlddata",
                 "remind2", "remulator", "rmndt", "rOpenscmRunner", "shinyresults",
                 "SPARQL", "trafficlight")
-  return(tryCatch({
+  # create output object
+  out <- tryCatch({
     sort(sub("^Package: ", "", grep("^Package: ", readLines(packagesUrl), value = TRUE)))
-  }, warning = function(w) fallback, error = function(e) fallback))
+  }, warning = function(w) fallback, error = function(e) fallback)
+
+  if(version) {
+    out <- cbind(package = out, 'pik-piam-version' = tryCatch({
+      sort(sub("^Version: ", "", grep("^Version: ", readLines(packagesUrl), value = TRUE)))
+    }, warning = function(w) fallback, error = function(e) fallback))
+  }
+  
+  return(out)
 }
